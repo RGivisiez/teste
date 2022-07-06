6919d1cff44be6f869af0b62830a479507462f27
RGivisiez <ronaldogivisiez@gmail.com>
2022-07-06 15:37:34 -0300

    .

--- JS/script.js
+++ JS/script.js
@@ -15,8 +15,6 @@
  *=============================================================================
  */
 
- import * as tf from '@tensorflow/tfjs';
-
  const video = document.getElementById('webcam');
  const liveView = document.getElementById('liveView');
  const demosSection = document.getElementById('demos');
@@ -24,7 +22,7 @@
  
  async function tensorFlow(){
         
-  const a = await tf.loadLayersModel('https://raw.githubusercontent.com/dkreider/tensorflowjs-cat-vs-dog/master/trained-model/model.json');
+  const a = await loadLayersModel('https://raw.githubusercontent.com/dkreider/tensorflowjs-cat-vs-dog/master/trained-model/model.json');
   console.log(a.summary());
   
   // const b = await tf.loadLayersModel('https://github.com/dkreider/tensorflowjs-cat-vs-dog/blob/master/trained-model/model.json');    
