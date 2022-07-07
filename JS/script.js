effcb6d0d2ddd435808c8bb2bdcd33e894dd3a10
RGivisiez <ronaldogivisiez@gmail.com>
2022-07-06 21:15:51 -0300

    teste hand

--- JS/script.js
+++ JS/script.js
@@ -1,127 +1,38 @@
-/**
- * @license
- * Copyright 2018 Google LLC. All Rights Reserved.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *=============================================================================
- */
+const videoElement = document.getElementsByClassName('input_video')[0];
+const canvasElement = document.getElementsByClassName('output_canvas')[0];
+const canvasCtx = canvasElement.getContext('2d');
 
- const video = document.getElementById('webcam');
- const liveView = document.getElementById('liveView');
- const demosSection = document.getElementById('demos');
- const enableWebcamButton = document.getElementById('webcamButton');
- 
- async function tensorFlow(){
-        
-  const a = await tf.loadLayersModel('https://raw.githubusercontent.com/dkreider/tensorflowjs-cat-vs-dog/master/trained-model/model.json');
-  console.log(a.summary());
-  console.log('--------');
-  const b = await tf.loadLayersModel('https://github.com/dkreider/tensorflowjs-cat-vs-dog/blob/master/trained-model/model.json');    
-  console.log(b.summary());
-
+function onResults(results) {
+  canvasCtx.save();
+  canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+  canvasCtx.drawImage(
+      results.image, 0, 0, canvasElement.width, canvasElement.height);
+  if (results.multiHandLandmarks) {
+    for (const landmarks of results.multiHandLandmarks) {
+      drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS,
+                     {color: '#00FF00', lineWidth: 5});
+      drawLandmarks(canvasCtx, landmarks, {color: '#FF0000', lineWidth: 2});
+    }
+  }
+  canvasCtx.restore();
 }
 
-tensorFlow();
+const hands = new Hands({locateFile: (file) => {
+  return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
+}});
+hands.setOptions({
+  maxNumHands: 2,
+  modelComplexity: 1,
+  minDetectionConfidence: 0.5,
+  minTrackingConfidence: 0.5
+});
+hands.onResults(onResults);
 
- // Check if webcam access is supported.
- function getUserMediaSupported() {
-   return !!(navigator.mediaDevices &&
-     navigator.mediaDevices.getUserMedia);
- }
- 
- // If webcam supported, add event listener to button for when user
- // wants to activate it to call enableCam function which we will 
- // define in the next step.
- if (getUserMediaSupported()) {
-   enableWebcamButton.addEventListener('click', enableCam);
- } else {
-   console.warn('getUserMedia() is not supported by your browser');
- }
-
- // Enable the live webcam view and start classification.
- function enableCam(event) {
-   // Only continue if the COCO-SSD has finished loading.
-   if (!model) {
-     return;
-   }
-   
-   // Hide the button once clicked.
-   event.target.classList.add('removed');  
-   
-   // getUsermedia parameters to force video but not audio.
-   const constraints = {
-     video: true
-   };
- 
-   // Activate the webcam stream.
-   navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
-     video.srcObject = stream;
-     video.addEventListener('loadeddata', predictWebcam);
-   });
- }
- 
- // Store the resulting model in the global scope of our app.
- var model = undefined;
- 
- // Before we can use COCO-SSD class we must wait for it to finish
- // loading. Machine Learning models can be large and take a moment 
- // to get everything needed to run.
- // Note: cocoSsd is an external object loaded from our index.html
- // script tag import so ignore any warning in Glitch.
- cocoSsd.load().then(function (loadedModel) {
-   model = loadedModel;
-   // Show demo section now model is ready to use.
-   demosSection.classList.remove('invisible');
- });
-
- var children = [];
- 
- function predictWebcam() {
-   // Now let's start classifying a frame in the stream.
-   model.detect(video).then(function (predictions) {
-     // Remove any highlighting we did previous frame.
-     for (let i = 0; i < children.length; i++) {
-       liveView.removeChild(children[i]);
-     }
-     children.splice(0);
-     
-     // Now lets loop through predictions and draw them to the live view if
-     // they have a high confidence score.
-     for (let n = 0; n < predictions.length; n++) {
-       // If we are over 66% sure we are sure we classified it right, draw it!
-       if (predictions[n].score > 0.66) {
-         const p = document.createElement('p');
-         p.innerText = predictions[n].class  + ' - with ' 
-             + Math.round(parseFloat(predictions[n].score) * 100) 
-             + '% confidence.';
-         p.style = 'margin-left: ' + predictions[n].bbox[0] + 'px; margin-top: '
-             + (predictions[n].bbox[1] - 10) + 'px; width: ' 
-             + (predictions[n].bbox[2] - 10) + 'px; top: 0; left: 0;';
- 
-         const highlighter = document.createElement('div');
-         highlighter.setAttribute('class', 'highlighter');
-         highlighter.style = 'left: ' + predictions[n].bbox[0] + 'px; top: '
-             + predictions[n].bbox[1] + 'px; width: ' 
-             + predictions[n].bbox[2] + 'px; height: '
-             + predictions[n].bbox[3] + 'px;';
- 
-         liveView.appendChild(highlighter);
-         liveView.appendChild(p);
-         children.push(highlighter);
-         children.push(p);
-       }
-     }
-     
-     // Call this function again to keep predicting when the browser is ready.
-     window.requestAnimationFrame(predictWebcam);
-   });
- }
+const camera = new Camera(videoElement, {
+  onFrame: async () => {
+    await hands.send({image: videoElement});
+  },
+  width: 1280,
+  height: 720
+});
+camera.start();
